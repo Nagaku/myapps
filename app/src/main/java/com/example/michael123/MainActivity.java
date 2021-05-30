8af37f57d7658bf2e6d09ddbb4b9747d36c038fe
mimel <mimelnagaku1@gmail.com>
2021-05-30 21:53:21 +0700

    profile done

--- app/src/main/java/com/example/michael123/MainActivity.java
+++ app/src/main/java/com/example/michael123/MainActivity.java
@@ -2,18 +2,13 @@ package com.example.michael123;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDialog;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.FrameLayout;
 
 import com.example.michael123.fragment.*;
@@ -21,7 +16,6 @@ import com.example.michael123.fragment.*;
 import com.example.michael123.model.Gallery;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -42,10 +36,13 @@ public class MainActivity extends AppCompatActivity{
         navigation = findViewById(R.id.bottomNavigationView);
         final Fragment fragment1 = new FragmentHome();
         final Fragment fragment2 = new FragmentDaily();
-        //populate();
         final Fragment fragment3 = new FragmentGallery(this.gallery);
+        final Fragment fragment4 = new FragmentMusic();
+        final Fragment fragment5 = new FragmentProfile();
         final FragmentManager fm = getSupportFragmentManager();
         active = fragment1;
+        fm.beginTransaction().add(R.id.frame, fragment5, "5").hide(fragment5).commit();
+        fm.beginTransaction().add(R.id.frame, fragment4, "4").hide(fragment4).commit();
         fm.beginTransaction().add(R.id.frame, fragment3, "3").hide(fragment3).commit();
         fm.beginTransaction().add(R.id.frame, fragment2, "2").hide(fragment2).commit();
         fm.beginTransaction().add(R.id.frame, fragment1, "1").commit();
@@ -65,7 +62,15 @@ public class MainActivity extends AppCompatActivity{
                         return true;
                     case R.id.menu_3:
                         fm.beginTransaction().hide(active).show(fragment3).commit();
-                        active = fragment2;
+                        active = fragment3;
+                        return true;
+                    case R.id.menu_4:
+                        fm.beginTransaction().hide(active).show(fragment4).commit();
+                        active = fragment4;
+                        return true;
+                    case R.id.menu_5:
+                        fm.beginTransaction().hide(active).show(fragment5).commit();
+                        active = fragment5;
                         return true;
                 }
                 return false;
---
+++ app/src/main/java/com/example/michael123/fragment/FragmentProfile.java
@@ -0,0 +1,137 @@
+package com.example.michael123.fragment;
+
+import android.Manifest;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatDialog;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.example.michael123.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link FragmentProfile#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class FragmentProfile extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    AppCompatDialog version;
+
+    public FragmentProfile() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment FragmentProfile.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static FragmentProfile newInstance(String param1, String param2) {
+        FragmentProfile fragment = new FragmentProfile();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    public void showVersion(View view) {
+        version = new AppCompatDialog(view.getContext());
+        version.setContentView(R.layout.version_pop);
+        version.setTitle("About this application");
+
+        if (!version.isShowing())
+            version.show();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        Button button = (Button) view.findViewById(R.id.callme);
+        Button btnm = (Button) view.findViewById(R.id.mailme);
+        Button btni = (Button) view.findViewById(R.id.followme);
+        Button btnf = (Button) view.findViewById(R.id.findme);
+        Button btnv = (Button) view.findViewById(R.id.version);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                String phone = "081338141194";
+                Intent callIntent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + phone));
+
+                startActivity(callIntent);
+            }
+        });
+
+        btnm.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                try{
+                    Intent intent = new Intent (Intent.ACTION_VIEW , Uri.parse("mailto:" + "michael.10118008@mahasiswa.unikom.ac.id"));
+                    intent.putExtra(Intent.EXTRA_SUBJECT, "Android");
+                    startActivity(intent);
+                }catch(ActivityNotFoundException e){
+                    //TODO smth
+                }
+            }
+        });
+
+        btni.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.instagram.com/mimelnagaku/")));
+            }
+        });
+
+
+        btnf.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://goo.gl/maps/W75p9s7XyBRt3bYc6")));
+            }
+        });
+
+
+        btnv.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                showVersion(arg0);
+            }
+        });
+
+        return view;
+    }
+
+}
